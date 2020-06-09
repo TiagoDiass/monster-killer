eb0f859f8bfc71f69f5aa11d93d957d1b434e9dd
tiagodias.nsc <tiago.dias@nscinfo.com.br>
2020-06-09 13:36:11 -0300

    :tada: First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+new Vue({
+  el: '#app',
+  data: () => ({
+    running: false,
+    playerLife: 100,
+    monsterLife: 0
+  }),
+
+  computed: {
+    hasResult() {
+      return this.playerLife == 0 || this.monsterLife == 0
+    }
+  },
+
+  methods: {
+
+  },
+
+  watch: {
+
+  }
+});
